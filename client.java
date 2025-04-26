1ef2f2759519e4caca08fcaf75a223db3160f09c
Yu38324 <152956746+Yu38324@users.noreply.github.com>
2025-04-26 18:33:38 +0800

    get IP

--- client.java
+++ client.java
@@ -1,6 +1,17 @@
-public class client {
-    void socket() {
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
+public class client {
+    //get client's IP address and host name
+    void socket() {
+        try{
+            InetAddress c = InetAddress.getByName("localhost"); 
+            System.out.println("InetAddress: " + c.getHostAddress());
+            System.out.println("InetAddress: " + c.getHostName()); 
+        } catch(UnknownHostException e) {
+            System.out.println("UnknownHostException: " + e.getMessage());
+        }
+        
     }
     void connect() {
 
--- serve.java
+++ serve.java
@@ -1,5 +1,16 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class serve {
+    //get serve's IP address and host name
     void socket() {
+        try{
+            InetAddress s = InetAddress.getByName("localhost"); 
+            System.out.println("InetAddress: " + s.getHostAddress());
+            System.out.println("InetAddress: " + s.getHostName()); 
+        } catch(UnknownHostException e) {
+            System.out.println("UnknownHostException: " + e.getMessage());
+        }
         
     }
      void bind() {
@@ -12,6 +23,7 @@ public class serve {
         
     }
     void recv() {
+        
         
     }
     String send() {
